0d45505d883b36deb1a21b9c8f53ebd82e74beab
Jmwofford <wofford.codes@gmail.com>
2021-04-19 15:28:56 -0500

    styleSheet-MedQueries:

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     get '/' => 'users#index'
-    # post '/sessions' => 'sessions#create'
-    # post '/users' => 'users#create'
-    # get '/dashboard' => 'profiles#index'
+    post '/sessions' => 'sessions#create'
+    post '/users' => 'users#create'
+    get '/dashboard' => 'profiles#index'
 end
