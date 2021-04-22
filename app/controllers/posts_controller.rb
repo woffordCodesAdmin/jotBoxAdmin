7a9a346879c19b35646ca15b4c732d4d9ce237b8
Jmwofford <wofford.codes@gmail.com>
2021-04-21 22:06:56 -0500

    uploadHeroku:

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,7 +51,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to '/', notice: "Post was successfully destroyed." }
       format.json { head :no_content }
     end
   end
